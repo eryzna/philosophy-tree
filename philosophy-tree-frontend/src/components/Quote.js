21f4dcc7090ad847b059b941bd609c97bb91b2ca
Elana R <eryzna@gmail.com>
2021-11-12 20:30:45 -0500

    package.json

--- philosophy-tree-frontend/src/components/Quote.js
+++ philosophy-tree-frontend/src/components/Quote.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
-//import QuoteContainer from './QuoteContainer.js'
 
 export default class Quote extends Component {
-
-  state = {
-    counter: 0
-  }
-
-  handleClick = () => {
-    this.setState ({
-      counter: this.state.counter + 1
-    })
-  }
     
     render() {
         
@@ -21,11 +10,7 @@ export default class Quote extends Component {
         
             <h1>{this.props.content}</h1>
             <p>{this.props.author}</p>
-            <button onClick = {this.handleClick}>Like</button>
-            <p>{this.state.counter}</p>
             
-
-           
         </div>
       )
     }
@@ -35,12 +20,18 @@ export default class Quote extends Component {
       quotes: []
     }
 
-    //<div key={id}>
-               // <><h1>{quote.content}</h1><p>{quote.author}</p></>
-           // </div>
+    //like feature
 
-  ////{this.props.quotes.map((quote, id) => 
-  //  <div key={id}>
-  //      <><h1>{quote.content}</h1><p>{quote.author}</p></>
-  //  </div>
-  //)}
+    //state = {
+    //  counter: 0
+    //}
+  //
+    //handleClick = () => {
+    //  this.setState ({
+    //    counter: this.state.counter + 1
+    //  })
+    //}
+    //<button className="btn" onClick = {this.handleClick}>Like</button>
+
+    //<p>{this.state.counter}</p>
+
